68fcdd871a36f7c8f329ca9d237762c572c0c35b
Jakob Ovrum <jakobovrum@gmail.com>
2013-02-15 16:19:48 +0100

    oops, calling convention

--- ssl/openssl.d
+++ ssl/openssl.d
@@ -8,20 +8,23 @@ struct SSL_CTX;
 struct SSL;
 struct SSL_METHOD;
 
-int function() SSL_library_init;
-void function() OPENSSL_add_all_algorithms_noconf;
-void function() SSL_load_error_strings;
+extern(C)
+{
+	int function() SSL_library_init;
+	void function() OPENSSL_add_all_algorithms_noconf;
+	void function() SSL_load_error_strings;
 
-char* function(c_ulong e, char* buf) ERR_error_string;
+	char* function(c_ulong e, char* buf) ERR_error_string;
 
-SSL_CTX* function(const(SSL_METHOD)* meth) SSL_CTX_new;
-const(SSL_METHOD)* function() SSLv3_client_method; /* SSLv3 */
+	SSL_CTX* function(const(SSL_METHOD)* meth) SSL_CTX_new;
+	const(SSL_METHOD)* function() SSLv3_client_method; /* SSLv3 */
 
-SSL* function(SSL_CTX* ctx) SSL_new;
-int function(SSL* s, int fd) SSL_set_fd;
-int function(SSL* ssl) SSL_connect;
-int function(SSL* ssl,void* buf,int num) SSL_read;
-int function(SSL* ssl,const(void)* buf,int num) SSL_write;
+	SSL* function(SSL_CTX* ctx) SSL_new;
+	int function(SSL* s, int fd) SSL_set_fd;
+	int function(SSL* ssl) SSL_connect;
+	int function(SSL* ssl,void* buf,int num) SSL_read;
+	int function(SSL* ssl,const(void)* buf,int num) SSL_write;
+}
 
 void loadOpenSSL()
 {
